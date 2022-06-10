db135daba48ba9864995ad888ba1102cdf85c61c
TarkosJordan <tarsis.orsi@gmail.com>
2022-06-09 21:57:01 -0300

    Implementação de execução de processamento da imagem com processos

--- op_bmp.c
+++ op_bmp.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/shm.h>
+#include <sys/wait.h>
 #include "./libs/matlib.h"
 
 
@@ -29,6 +30,12 @@ int main(int argc, char** argv){
     FILE *arquivoEntrada = fopen(argv[1], "rb"); 
     if(arquivoEntrada == NULL){
         printf("Erro ao abrir o arquivo %s\n", argv[1]);
+        exit(0);
+    }
+    
+    FILE *arquivoSaida = fopen(argv[2], "wb");
+    if(arquivoSaida == NULL){
+        printf("Erro ao abrir o arquivo %s\n", argv[2]);
         exit(0);
     }
 
@@ -70,63 +77,62 @@ int main(int argc, char** argv){
 
     if(id == 0) //  Processo pai
     {
-        
-    }
-    FILE *arquivoSaida = fopen(argv[2], "wb");
-    if(arquivoSaida == NULL){
-        printf("Erro ao abrir o arquivo %s\n", argv[2]);
-        exit(0);
-    }
+    	for (processIterator = 1; processIterator < numeroProcessos; processIterator++) {
+            wait(NULL);
+	    }
 
-    fwrite(&cabecalhoImg, sizeof(CABECALHO), 1, arquivoSaida);
-    for(i=0; i<alturaImagem; i++){
-        for(j=0; j<larguraImagem; j++){
-            fwrite(&img(i,j), sizeof(PIXEL), 1, arquivoSaida);
+        fwrite(&cabecalhoImg, sizeof(CABECALHO), 1, arquivoSaida);
+        for(i=0; i<alturaImagem; i++){
+            for(j=0; j<larguraImagem; j++){
+                fwrite(&img(i,j), sizeof(PIXEL), 1, arquivoSaida);
+            }
         }
-    }
-    
-    // Tem que realizar um filtro pra testar se o numero de colunas eh multiplo de 4 e caso nao seja descontar o resto das operacoes realizadas na imagem
-    // no final tem que somar esse resto de volta no arquivo de saida para conservar a estrutura da imagem 
-    // eh do padrao do tipo do arquivo bmp usar multiplo de 4 nas linhas 
+        // Tem que realizar um filtro pra testar se o numero de colunas eh multiplo de 4 e caso nao seja descontar o resto das operacoes realizadas na imagem
+        // no final tem que somar esse resto de volta no arquivo de saida para conservar a estrutura da imagem 
+        // eh do padrao do tipo do arquivo bmp usar multiplo de 4 nas linhas 
 
-    char aux;
+        char aux;
 
-    for(i=0; i<alturaImagem; i++){
+        for(i=0; i<alturaImagem; i++){
 
-		int ali = (larguraImagem * 3) % 4;
+            int ali = (larguraImagem * 3) % 4;
 
-		if (ali != 0){
-			ali = 4 - ali;
-		}
+            if (ali != 0){
+                ali = 4 - ali;
+            }
 
-		for(j=0; j<larguraImagem; j++){
-			fread(&img(i,j), sizeof(PIXEL), 1, arquivoEntrada);
+            for(j=0; j<larguraImagem; j++){
+                fread(&img(i,j), sizeof(PIXEL), 1, arquivoEntrada);
+            }
+            
+            for(j=0; j<ali; j++){
+                fread(&aux, sizeof(unsigned char), 1, arquivoEntrada);
+            }
         }
-        
-        for(j=0; j<ali; j++){
-			fread(&aux, sizeof(unsigned char), 1, arquivoEntrada);
-    	}
-    }
 
-    for(i=0; i<alturaImagem; i++){
+        for(i=0; i<alturaImagem; i++){
 
-		int ali = (larguraImagem * 3) % 4;
+            int ali = (larguraImagem * 3) % 4;
 
-		if (ali != 0){
-			ali = 4 - ali;
-		}
+            if (ali != 0){
+                ali = 4 - ali;
+            }
 
-		for(j=0; j<larguraImagem; j++){
-	        fwrite(&img(i,j), sizeof(PIXEL), 1, arquivoSaida);
+            for(j=0; j<larguraImagem; j++){
+                fwrite(&img(i,j), sizeof(PIXEL), 1, arquivoSaida);
+            }
+            
+            for(j=0; j<ali; j++){
+                fwrite(&aux, sizeof(unsigned char), 1, arquivoSaida);
+            }
         }
-        
-        for(j=0; j<ali; j++){
-	        fwrite(&aux, sizeof(unsigned char), 1, arquivoSaida);
-    	}
+        shmdt(img);
+	    shmctl(shmid, IPC_RMID, 0);   
+    } 
+    else    //Processos Filhos
+    {
+        shmdt(img);
     }
-    
-    // Desalocando memoria dos mallocs
-    //free(img);
 
     //Fechamento dos arquivos    
     fclose(arquivoEntrada);
